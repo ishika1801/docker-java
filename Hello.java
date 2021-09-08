422b40486f92e8c592d917c0e1d59de44981f535
sujataoak799 <79905110+sujataoak799@users.noreply.github.com>
2021-09-08 08:56:34 +0000

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello{
 public static void main(String[] args){
-System.out.println("Hello Evryone!!!");
+System.out.println("Hello Everyone!!!");
 System.out.println("I am a java app running inside a docker container");
 }
 }
